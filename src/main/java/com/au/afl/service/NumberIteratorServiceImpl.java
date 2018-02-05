21e096c8d4e92681ff3be13db0f21877e09468d3
Jack Lord Hermoso <jack.lord.hermoso@gmail.com>
2018-02-05 14:27:38 +1000

    Update NumberIteratorServiceImpl.java

--- src/main/java/com/au/afl/service/NumberIteratorServiceImpl.java
+++ src/main/java/com/au/afl/service/NumberIteratorServiceImpl.java
@@ -7,37 +7,37 @@ import com.au.afl.util.AFLUtil;
 
 public class NumberIteratorServiceImpl implements NumberIteratorService{
 
-	
-	/**
-	 * This is the service that will execute the number
-	 * iterator
-	 * @throws AFLException 
-	 */ 
-	@Override
-	public StringBuilder executeNumberIterator(int integersLength, int firstNumber, int secondNumber) throws AFLException {
-		
-		StringBuilder resultMessage;
-		try {
-			resultMessage = new StringBuilder();
-			for(int iNext = 0; iNext < integersLength; iNext++){
-			    
-				AFLEnums firstDivisible = AFLUtil.validateNumber(firstNumber, iNext);
-				AFLEnums secondDivisible = AFLUtil.validateNumber(secondNumber, iNext);
-			    
-			    if(firstDivisible.equals(AFLEnums.DIVISIBLE_THREE) 
-			    		&& secondDivisible.equals(AFLEnums.DIVISIBLE_FIVE)) {
-			    	resultMessage.append(iNext + AFLConstants.BAZ_MESSAGE + "\n");
-			    }else if(firstDivisible.equals(AFLEnums.DIVISIBLE_THREE)){
-			    	resultMessage.append(iNext + AFLConstants.FOO_MESSAGE + "\n");
-			    }else if(secondDivisible.equals(AFLEnums.DIVISIBLE_FIVE)){
-			    	resultMessage.append(iNext + AFLConstants.BAR_MESSAGE + "\n");
-			    }else{
-			    	resultMessage.append(iNext + "\n");
-			    }
-			}
-		} catch (Exception e) {
-			throw new AFLException("The system encountered error");
-		}
-		return resultMessage;
-	}
+
+    /**
+     * This is the service that will execute the number
+     * iterator
+     * @throws AFLException
+     */
+    @Override
+    public StringBuilder executeNumberIterator(int integersLength, int firstNumber, int secondNumber) throws AFLException {
+
+        StringBuilder resultMessage;
+        try {
+            resultMessage = new StringBuilder();
+            for(int iNext = 1; iNext <= integersLength; iNext++){
+
+                AFLEnums firstDivisible = AFLUtil.validateNumber(firstNumber, iNext);
+                AFLEnums secondDivisible = AFLUtil.validateNumber(secondNumber, iNext);
+
+                if(firstDivisible.equals(AFLEnums.DIVISIBLE_TRUE)
+                        && secondDivisible.equals(AFLEnums.DIVISIBLE_TRUE)) {
+                    resultMessage.append(iNext + AFLConstants.BAZ_MESSAGE + "\n");
+                }else if(firstDivisible.equals(AFLEnums.DIVISIBLE_TRUE)){
+                    resultMessage.append(iNext + AFLConstants.FOO_MESSAGE + "\n");
+                }else if(secondDivisible.equals(AFLEnums.DIVISIBLE_TRUE)){
+                    resultMessage.append(iNext + AFLConstants.BAR_MESSAGE + "\n");
+                }else{
+                    resultMessage.append(iNext + "\n");
+                }
+            }
+        } catch (Exception e) {
+            throw new AFLException("The system encountered error");
+        }
+        return resultMessage;
+    }
 }
