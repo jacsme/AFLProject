e4001d3b503735e9c3155a97f8b3c2c09130786e
Jack Lord Hermoso <jack.lord.hermoso@gmail.com>
2018-02-05 17:03:53 +1000

    Update TestConstantsResult.java

--- src/test/java/com/au/afl/constant/TestConstantsResult.java
+++ src/test/java/com/au/afl/constant/TestConstantsResult.java
@@ -1,19 +1,34 @@
 package com.au.afl.constant;
 
 public class TestConstantsResult {
-	
-	public static StringBuilder setResultMessageNumber(){
-		StringBuilder resultMessage = new StringBuilder();
-		resultMessage.append("0BAZ\n");
-		resultMessage.append("1\n");
-		resultMessage.append("2\n");
-		resultMessage.append("3FOO\n");
-		resultMessage.append("4\n");
-		resultMessage.append("5BAR\n");
-		resultMessage.append("6FOO\n");
-		resultMessage.append("7\n");
-		resultMessage.append("8\n");
-		resultMessage.append("9FOO\n");
-		return resultMessage;
-	}
+
+    public static StringBuilder setResultMessageOddNumber(){
+        StringBuilder resultMessage = new StringBuilder();
+        resultMessage.append("1\n");
+        resultMessage.append("2\n");
+        resultMessage.append("3FOO\n");
+        resultMessage.append("4\n");
+        resultMessage.append("5BAR\n");
+        resultMessage.append("6FOO\n");
+        resultMessage.append("7\n");
+        resultMessage.append("8\n");
+        resultMessage.append("9FOO\n");
+        resultMessage.append("10\n");
+        return resultMessage;
+    }
+    
+    public static StringBuilder setResultMessageEvenNumber(){
+        StringBuilder resultMessage = new StringBuilder();
+        resultMessage.append("1\n");
+        resultMessage.append("2FOO\n");
+        resultMessage.append("3\n");
+        resultMessage.append("4BAZ\n");
+        resultMessage.append("5\n");
+        resultMessage.append("6FOO\n");
+        resultMessage.append("7\n");
+        resultMessage.append("8BAZ\n");
+        resultMessage.append("9\n");
+        resultMessage.append("10FOO\n");
+        return resultMessage;
+    }
 }
