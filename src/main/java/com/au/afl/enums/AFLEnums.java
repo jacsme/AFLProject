2fbafcb101f266b91d6cf533232ed0335f86d38c
Jack Lord Hermoso <jack.lord.hermoso@gmail.com>
2018-02-05 14:26:36 +1000

    Update AFLEnums.java

--- src/main/java/com/au/afl/enums/AFLEnums.java
+++ src/main/java/com/au/afl/enums/AFLEnums.java
@@ -1,22 +1,14 @@
 package com.au.afl.enums;
 
 public enum AFLEnums {
-	
-	DIVISIBLE_ONE(1), 
-	DIVISIBLE_TWO(2), 
-	DIVISIBLE_THREE(3),
-	DIVISIBLE_FOUR(4),
-	DIVISIBLE_FIVE(5),
-	DIVISIBLE_SIX(6),
-	DIVISIBLE_SEVEN(7),
-	DIVISIBLE_EIGHT(8),
-	DIVISIBLE_NINE(9),
-	DIVISIBLE_NONE(0);
-	
-	private int value; 
-	private AFLEnums(int value) { 
-		this.value = value; 
-	}
+
+    DIVISIBLE_TRUE(true),
+    DIVISIBLE_FALSE(false);
+
+    private boolean value;
+    private AFLEnums(boolean value) {
+        this.value = value;
+    }
 
 
 }
